deb1deddca66466a13f5b40c44c9635b249bd52b
ray zee <raymo@PC.ZINGSERVER1.GOV>
2023-06-02 11:44:25 +0200

    migrations

--- database/migrations/2023_05_31_213203_create_categories_table.php
+++ database/migrations/2023_05_31_213203_create_categories_table.php
@@ -12,6 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('categories', function (Blueprint $table) {
+            
             $table->id('category_id');
             $table->string('category_name');
             $table->timestamps();//updated_at
